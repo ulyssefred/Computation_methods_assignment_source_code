2cc3fce57db3cf082c3aafb66e3552c01a7cbb20
ulysses <wx2806784370@outlook.com>
2021-11-15 21:10:16 +0800

    version 1.0

--- Computation_methods_assignment_source_code/main.cpp
+++ Computation_methods_assignment_source_code/main.cpp
@@ -3,19 +3,13 @@
 #include "LSI.h"
 #include "CN.h"
 
-#include "matplotlibcpp.h"
-namespace plt = matplotlibcpp;
-int main() {
-    plt::plot({ 1,3,2,4 });
-    plt::show();
+int main()
+{
+	DF test;
+	test.SetValue();
+	test.GetError();
+	LSI test2;
+	test2.SetValue();
+	test2.GetError();
+
 }
-//int main()
-//{
-//	DF test;
-//	test.SetValue();
-//	test.GetError();
-//	LSI test2;
-//	test2.SetValue();
-//	test2.GetError();
-//
-//}
